22f09e397b142db54c9227799aba03d70c2a9781
riulwoo <geun10121@naver.com>
2022-11-25 20:08:33 +0900

    직원호출 / 계산서 UI 추가

--- Torder/Torder/Form2.Designer.cs
+++ Torder/Torder/Form2.Designer.cs
@@ -28,19 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.gbTopbar = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnCall = new System.Windows.Forms.Button();
+            this.gbNeed = new System.Windows.Forms.GroupBox();
+            this.btnSoi = new System.Windows.Forms.Button();
+            this.btnWTissue = new System.Windows.Forms.Button();
+            this.btnSpoon = new System.Windows.Forms.Button();
+            this.btnChopsticks = new System.Windows.Forms.Button();
+            this.btnWater = new System.Windows.Forms.Button();
+            this.btnApron = new System.Windows.Forms.Button();
+            this.btnDish = new System.Windows.Forms.Button();
+            this.btnAddBroth = new System.Windows.Forms.Button();
+            this.gbTopbar.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // gbTopbar
+            // 
+            this.gbTopbar.Controls.Add(this.btnClose);
+            this.gbTopbar.Controls.Add(this.label1);
+            this.gbTopbar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.gbTopbar.Location = new System.Drawing.Point(0, 0);
+            this.gbTopbar.Name = "gbTopbar";
+            this.gbTopbar.Size = new System.Drawing.Size(700, 55);
+            this.gbTopbar.TabIndex = 0;
+            this.gbTopbar.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "직원호출 - 무엇을 도와드릴까요?";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(613, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 37);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "X 닫기";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnCall
+            // 
+            this.btnCall.Location = new System.Drawing.Point(18, 300);
+            this.btnCall.Name = "btnCall";
+            this.btnCall.Size = new System.Drawing.Size(393, 48);
+            this.btnCall.TabIndex = 2;
+            this.btnCall.Text = "직원만 호출하기";
+            this.btnCall.UseVisualStyleBackColor = true;
+            // 
+            // gbNeed
+            // 
+            this.gbNeed.Location = new System.Drawing.Point(434, 61);
+            this.gbNeed.Name = "gbNeed";
+            this.gbNeed.Size = new System.Drawing.Size(254, 287);
+            this.gbNeed.TabIndex = 3;
+            this.gbNeed.TabStop = false;
+            // 
+            // btnSoi
+            // 
+            this.btnSoi.Location = new System.Drawing.Point(18, 71);
+            this.btnSoi.Name = "btnSoi";
+            this.btnSoi.Size = new System.Drawing.Size(75, 64);
+            this.btnSoi.TabIndex = 4;
+            this.btnSoi.Text = "간장";
+            this.btnSoi.UseVisualStyleBackColor = true;
+            // 
+            // btnWTissue
+            // 
+            this.btnWTissue.Location = new System.Drawing.Point(126, 71);
+            this.btnWTissue.Name = "btnWTissue";
+            this.btnWTissue.Size = new System.Drawing.Size(75, 64);
+            this.btnWTissue.TabIndex = 5;
+            this.btnWTissue.Text = "물티슈";
+            this.btnWTissue.UseVisualStyleBackColor = true;
+            // 
+            // btnSpoon
+            // 
+            this.btnSpoon.Location = new System.Drawing.Point(231, 71);
+            this.btnSpoon.Name = "btnSpoon";
+            this.btnSpoon.Size = new System.Drawing.Size(75, 64);
+            this.btnSpoon.TabIndex = 6;
+            this.btnSpoon.Text = "숟가락";
+            this.btnSpoon.UseVisualStyleBackColor = true;
+            // 
+            // btnChopsticks
+            // 
+            this.btnChopsticks.Location = new System.Drawing.Point(336, 71);
+            this.btnChopsticks.Name = "btnChopsticks";
+            this.btnChopsticks.Size = new System.Drawing.Size(75, 64);
+            this.btnChopsticks.TabIndex = 7;
+            this.btnChopsticks.Text = "젓가락";
+            this.btnChopsticks.UseVisualStyleBackColor = true;
+            // 
+            // btnWater
+            // 
+            this.btnWater.Location = new System.Drawing.Point(18, 158);
+            this.btnWater.Name = "btnWater";
+            this.btnWater.Size = new System.Drawing.Size(75, 64);
+            this.btnWater.TabIndex = 8;
+            this.btnWater.Text = "물";
+            this.btnWater.UseVisualStyleBackColor = true;
+            // 
+            // btnApron
+            // 
+            this.btnApron.Location = new System.Drawing.Point(126, 158);
+            this.btnApron.Name = "btnApron";
+            this.btnApron.Size = new System.Drawing.Size(75, 64);
+            this.btnApron.TabIndex = 9;
+            this.btnApron.Text = "앞치마";
+            this.btnApron.UseVisualStyleBackColor = true;
+            // 
+            // btnDish
+            // 
+            this.btnDish.Location = new System.Drawing.Point(232, 158);
+            this.btnDish.Name = "btnDish";
+            this.btnDish.Size = new System.Drawing.Size(75, 64);
+            this.btnDish.TabIndex = 10;
+            this.btnDish.Text = "접시";
+            this.btnDish.UseVisualStyleBackColor = true;
+            // 
+            // btnAddBroth
+            // 
+            this.btnAddBroth.Location = new System.Drawing.Point(336, 158);
+            this.btnAddBroth.Name = "btnAddBroth";
+            this.btnAddBroth.Size = new System.Drawing.Size(75, 64);
+            this.btnAddBroth.TabIndex = 11;
+            this.btnAddBroth.Text = "육수 추가";
+            this.btnAddBroth.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 360);
+            this.Controls.Add(this.btnAddBroth);
+            this.Controls.Add(this.btnDish);
+            this.Controls.Add(this.btnApron);
+            this.Controls.Add(this.btnWater);
+            this.Controls.Add(this.btnChopsticks);
+            this.Controls.Add(this.btnSpoon);
+            this.Controls.Add(this.btnWTissue);
+            this.Controls.Add(this.btnSoi);
+            this.Controls.Add(this.gbNeed);
+            this.Controls.Add(this.btnCall);
+            this.Controls.Add(this.gbTopbar);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form2";
             this.Text = "직원 호출";
+            this.gbTopbar.ResumeLayout(false);
+            this.gbTopbar.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbTopbar;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCall;
+        private System.Windows.Forms.GroupBox gbNeed;
+        private System.Windows.Forms.Button btnSoi;
+        private System.Windows.Forms.Button btnWTissue;
+        private System.Windows.Forms.Button btnSpoon;
+        private System.Windows.Forms.Button btnChopsticks;
+        private System.Windows.Forms.Button btnWater;
+        private System.Windows.Forms.Button btnApron;
+        private System.Windows.Forms.Button btnDish;
+        private System.Windows.Forms.Button btnAddBroth;
     }
 }
--- Torder/Torder/Form3.Designer.cs
+++ Torder/Torder/Form3.Designer.cs
@@ -28,21 +28,195 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblBill = new System.Windows.Forms.Label();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.gbSum = new System.Windows.Forms.GroupBox();
+            this.gbDut = new System.Windows.Forms.GroupBox();
+            this.lblDutCost = new System.Windows.Forms.Label();
+            this.lblDutNum = new System.Windows.Forms.Label();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.lblNum = new System.Windows.Forms.Label();
+            this.lblTextDut = new System.Windows.Forms.Label();
+            this.lblTextSum = new System.Windows.Forms.Label();
+            this.lblSumCost = new System.Windows.Forms.Label();
+            this.gbSum.SuspendLayout();
+            this.gbDut.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // lblBill
+            // 
+            this.lblBill.AutoSize = true;
+            this.lblBill.Location = new System.Drawing.Point(21, 14);
+            this.lblBill.Name = "lblBill";
+            this.lblBill.Size = new System.Drawing.Size(41, 12);
+            this.lblBill.TabIndex = 0;
+            this.lblBill.Text = "계산서";
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(479, 14);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(101, 12);
+            this.lblTimer.TabIndex = 1;
+            this.lblTimer.Text = "초 후에 닫힙니다.";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(606, 9);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "닫기";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(23, 60);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(442, 210);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // gbSum
+            // 
+            this.gbSum.Controls.Add(this.lblSumCost);
+            this.gbSum.Controls.Add(this.lblTextSum);
+            this.gbSum.Location = new System.Drawing.Point(23, 276);
+            this.gbSum.Name = "gbSum";
+            this.gbSum.Size = new System.Drawing.Size(442, 56);
+            this.gbSum.TabIndex = 4;
+            this.gbSum.TabStop = false;
+            // 
+            // gbDut
+            // 
+            this.gbDut.Controls.Add(this.lblDutCost);
+            this.gbDut.Controls.Add(this.lblDutNum);
+            this.gbDut.Controls.Add(this.btnPlus);
+            this.gbDut.Controls.Add(this.btnMinus);
+            this.gbDut.Controls.Add(this.lblNum);
+            this.gbDut.Controls.Add(this.lblTextDut);
+            this.gbDut.Location = new System.Drawing.Point(481, 55);
+            this.gbDut.Name = "gbDut";
+            this.gbDut.Size = new System.Drawing.Size(200, 277);
+            this.gbDut.TabIndex = 5;
+            this.gbDut.TabStop = false;
+            // 
+            // lblDutCost
+            // 
+            this.lblDutCost.AutoSize = true;
+            this.lblDutCost.Location = new System.Drawing.Point(94, 221);
+            this.lblDutCost.Name = "lblDutCost";
+            this.lblDutCost.Size = new System.Drawing.Size(17, 12);
+            this.lblDutCost.TabIndex = 11;
+            this.lblDutCost.Text = "원";
+            this.lblDutCost.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDutNum
+            // 
+            this.lblDutNum.AutoSize = true;
+            this.lblDutNum.Location = new System.Drawing.Point(81, 175);
+            this.lblDutNum.Name = "lblDutNum";
+            this.lblDutNum.Size = new System.Drawing.Size(39, 12);
+            this.lblDutNum.TabIndex = 10;
+            this.lblDutNum.Text = "1 인당";
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(147, 93);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(30, 23);
+            this.btnPlus.TabIndex = 9;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Location = new System.Drawing.Point(24, 93);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(30, 23);
+            this.btnMinus.TabIndex = 8;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            // 
+            // lblNum
+            // 
+            this.lblNum.AutoSize = true;
+            this.lblNum.Location = new System.Drawing.Point(94, 98);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(11, 12);
+            this.lblNum.TabIndex = 7;
+            this.lblNum.Text = "1";
+            // 
+            // lblTextDut
+            // 
+            this.lblTextDut.AutoSize = true;
+            this.lblTextDut.Location = new System.Drawing.Point(71, 36);
+            this.lblTextDut.Name = "lblTextDut";
+            this.lblTextDut.Size = new System.Drawing.Size(53, 12);
+            this.lblTextDut.TabIndex = 6;
+            this.lblTextDut.Text = "더치페이";
+            // 
+            // lblTextSum
+            // 
+            this.lblTextSum.AutoSize = true;
+            this.lblTextSum.Location = new System.Drawing.Point(15, 27);
+            this.lblTextSum.Name = "lblTextSum";
+            this.lblTextSum.Size = new System.Drawing.Size(29, 12);
+            this.lblTextSum.TabIndex = 0;
+            this.lblTextSum.Text = "합계";
+            // 
+            // lblSumCost
+            // 
+            this.lblSumCost.AutoSize = true;
+            this.lblSumCost.Location = new System.Drawing.Point(396, 27);
+            this.lblSumCost.Name = "lblSumCost";
+            this.lblSumCost.Size = new System.Drawing.Size(17, 12);
+            this.lblSumCost.TabIndex = 1;
+            this.lblSumCost.Text = "원";
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(700, 360);
+            this.Controls.Add(this.gbDut);
+            this.Controls.Add(this.gbSum);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.lblBill);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form3";
             this.Text = "계산서";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.gbSum.ResumeLayout(false);
+            this.gbSum.PerformLayout();
+            this.gbDut.ResumeLayout(false);
+            this.gbDut.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblBill;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.GroupBox gbSum;
+        private System.Windows.Forms.GroupBox gbDut;
+        private System.Windows.Forms.Label lblDutCost;
+        private System.Windows.Forms.Label lblDutNum;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Label lblNum;
+        private System.Windows.Forms.Label lblTextDut;
+        private System.Windows.Forms.Label lblSumCost;
+        private System.Windows.Forms.Label lblTextSum;
     }
 }
