a1eb131b7bce4c444017aef538176810771e6ccd
hydrian634 <hydrian634@naver.com>
2022-12-03 23:51:02 +0900

    수정X

--- Torder/Torder/Form8.Designer.cs
+++ Torder/Torder/Form8.Designer.cs
@@ -35,16 +35,10 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.groupBox1.SuspendLayout();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lb_totalcount
@@ -108,49 +102,10 @@
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.label6);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.panel2);
             this.panel1.Location = new System.Drawing.Point(51, 64);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(668, 331);
             this.panel1.TabIndex = 33;
-            // 
-            // panel2
-            // 
-            this.panel2.Location = new System.Drawing.Point(441, 35);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 100);
-            this.panel2.TabIndex = 0;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(328, 112);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(256, 62);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(170, 12);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "이 느낌? ㅇㅋ받으면 ID값 고침";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(439, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(351, 12);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "상단 바를 그룹박스로? 아니면 일일정산 글씨 큰거 얘기하는건가";
             // 
             // label3
             // 
@@ -170,43 +125,34 @@
             this.label4.TabIndex = 35;
             this.label4.Text = "주문 내역";
             // 
-            // label5
+            // panel2
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(344, 179);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(171, 12);
-            this.label5.TabIndex = 3;
-            this.label5.Text = "근데 여기 어떻게 처리할 예정?";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(326, 221);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(249, 12);
-            this.label6.TabIndex = 4;
-            this.label6.Text = "내 기준에서는 더럽게 배치하는거밖에 없는데";
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(52, 64);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(668, 49);
+            this.panel2.TabIndex = 0;
             // 
             // Form8
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(802, 458);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btn_daily);
             this.Controls.Add(this.btn_orderlist);
             this.Name = "Form8";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "일일 정산";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -221,13 +167,8 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel2;
     }
 }
