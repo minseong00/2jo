543202e00dd6b6887eca0a8d693e278a92cf7f85
hydrian634 <hydrian634@naver.com>
2022-12-04 20:29:52 +0900

    합계 날아갔길래 그거 넣어둠.

--- Torder/Torder/Form7.Designer.cs
+++ Torder/Torder/Form7.Designer.cs
@@ -31,6 +31,11 @@
             this.admin_pn_daily = new System.Windows.Forms.Panel();
             this.admin_lb_daily = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // admin_pn_daily
@@ -55,10 +60,43 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.groupBox1);
             this.panel1.Location = new System.Drawing.Point(-1, 315);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(702, 46);
             this.panel1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(13, -2);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(0);
+            this.groupBox1.Size = new System.Drawing.Size(676, 42);
+            this.groupBox1.TabIndex = 35;
+            this.groupBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 15F);
+            this.label3.Location = new System.Drawing.Point(631, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 28);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "원";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("맑은 고딕", 15F);
+            this.label4.Location = new System.Drawing.Point(6, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 28);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "합계";
             // 
             // Form7
             // 
@@ -75,6 +113,9 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "관리자_일일 정산";
             this.Load += new System.EventHandler(this.Form7_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -84,5 +125,8 @@
         private System.Windows.Forms.Panel admin_pn_daily;
         private System.Windows.Forms.Label admin_lb_daily;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
