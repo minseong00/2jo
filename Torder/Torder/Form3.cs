6e09e10042a4daa479dd6989e40737330be56375
riulwoo <geun10121@naver.com>
2022-12-04 21:40:55 +0900

    계산서 DB 연결 및 로직 추가

--- Torder/Torder/Form3.cs
+++ Torder/Torder/Form3.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace Torder
 {
@@ -16,6 +17,7 @@ namespace Torder
         {
             InitializeComponent();
         }
+        private string StrSQL = @"Provider=Microsoft.ACE.OLEDB.16.0; Data Source=2jo.accdb;Mode=ReadWrite";
 
         private void btnClose_Click(object sender, EventArgs e)
         {
@@ -23,13 +25,32 @@ namespace Torder
         }
         int closeTime = 29;
         int dutNum = 0;
+        int price = 0;
         private void Form3_Load(object sender, EventArgs e)
         {
-            this.MinimumSize = new Size(716, 399);
-            this.MaximumSize = new Size(716, 399);
-            // access에서 테이블의 주문목록 가져와서 lvView에 넣기
+            // 창꺼짐 타이머 시작
             time_close.Start();
             dutNum = int.Parse(this.lblNum.Text);
+            // access에서 테이블의 주문목록 가져와서 lvView에 넣기
+            // DB 연결 및 주문내역 조회
+            var Conn = new OleDbConnection(StrSQL);
+            Conn.Open();
+            var Comm = new OleDbCommand("SELECT * FROM order", Conn);
+            var myRead = Comm.ExecuteReader();
+            while(myRead.Read())
+            {
+                // 상품명, 수량, 주문일자, 총 가격
+                this.lv_bill.Items.Add(new ListViewItem(new string[] { myRead[0].ToString(), myRead[1].ToString(), myRead[2].ToString(), myRead[3].ToString() }));
+                price += Convert.ToInt32(myRead[3].ToString());
+            }
+            this.lb_amount.Text = price + "원";
+            this.lblDutCost.Text = price + "원";
+            
+            // 사이즈 조정
+            this.MinimumSize = new Size(716, 399);
+            this.MaximumSize = new Size(716, 399);
+
+            
         }
 
         private void time_close_Tick(object sender, EventArgs e)
@@ -44,7 +65,8 @@ namespace Torder
             {
                 dutNum--;
                 this.lblNum.Text = dutNum.ToString();
-                this.lblDutNum.Text = dutNum.ToString() + "인당";
+                this.lblDutNum.Text = dutNum.ToString() + " 인당";
+                this.lblDutCost.Text = price / dutNum + "원";
             }
 
         }
@@ -55,7 +77,8 @@ namespace Torder
             {
                 dutNum++;
                 this.lblNum.Text = dutNum.ToString();
-                this.lblDutNum.Text = dutNum.ToString() + "인당";
+                this.lblDutNum.Text = dutNum.ToString() + " 인당";
+                this.lblDutCost.Text = price / dutNum + "원";
             }
         }
     }
