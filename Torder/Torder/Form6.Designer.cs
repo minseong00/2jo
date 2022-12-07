65fed80d8f686aeb65e81bb2e8cb975d955d8c47
hydrian634 <hydrian634@naver.com>
2022-12-07 15:46:16 +0900

    form6 디자인 수정
    네 그렇습니다.

--- Torder/Torder/Form6.Designer.cs
+++ Torder/Torder/Form6.Designer.cs
@@ -31,12 +31,11 @@
             this.label1 = new System.Windows.Forms.Label();
             this.admin_lb_olist = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.admin_gb_amount = new System.Windows.Forms.GroupBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.listView1 = new System.Windows.Forms.ListView();
-            this.admin_gb_amount.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -61,30 +60,21 @@
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
             this.panel1.Location = new System.Drawing.Point(-1, 216);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(684, 46);
             this.panel1.TabIndex = 37;
             // 
-            // admin_gb_amount
-            // 
-            this.admin_gb_amount.Controls.Add(this.label3);
-            this.admin_gb_amount.Controls.Add(this.label4);
-            this.admin_gb_amount.Location = new System.Drawing.Point(11, 136);
-            this.admin_gb_amount.Margin = new System.Windows.Forms.Padding(0);
-            this.admin_gb_amount.Name = "admin_gb_amount";
-            this.admin_gb_amount.Padding = new System.Windows.Forms.Padding(0);
-            this.admin_gb_amount.Size = new System.Drawing.Size(671, 42);
-            this.admin_gb_amount.TabIndex = 6;
-            this.admin_gb_amount.TabStop = false;
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Silver;
+            this.label3.BackColor = System.Drawing.Color.Black;
             this.label3.Font = new System.Drawing.Font("맑은 고딕", 15F);
-            this.label3.Location = new System.Drawing.Point(631, 9);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(648, 9);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(32, 28);
             this.label3.TabIndex = 6;
@@ -93,9 +83,10 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Silver;
+            this.label4.BackColor = System.Drawing.Color.Black;
             this.label4.Font = new System.Drawing.Font("맑은 고딕", 15F);
-            this.label4.Location = new System.Drawing.Point(6, 9);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(12, 9);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(52, 28);
             this.label4.TabIndex = 0;
@@ -105,7 +96,6 @@
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel2.Controls.Add(this.admin_gb_amount);
             this.panel2.Controls.Add(this.listView1);
             this.panel2.Controls.Add(this.label1);
             this.panel2.Controls.Add(this.panel1);
@@ -119,6 +109,7 @@
             // 
             // listView1
             // 
+            this.listView1.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.listView1.HideSelection = false;
             this.listView1.Location = new System.Drawing.Point(-1, 45);
             this.listView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
@@ -140,8 +131,8 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "관리자_주문내역";
             this.Load += new System.EventHandler(this.Form6_Load);
-            this.admin_gb_amount.ResumeLayout(false);
-            this.admin_gb_amount.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.ResumeLayout(false);
@@ -151,7 +142,6 @@
         #endregion
         private System.Windows.Forms.Label admin_lb_olist;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.GroupBox admin_gb_amount;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Panel panel2;
