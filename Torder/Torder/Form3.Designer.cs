a5bd57ee50014fb7247e2d721e11dde43f10846e
hydrian634 <hydrian634@naver.com>
2022-12-07 15:07:22 +0900

    form3 수정

--- Torder/Torder/Form3.Designer.cs
+++ Torder/Torder/Form3.Designer.cs
@@ -32,7 +32,6 @@
             this.lblBill = new System.Windows.Forms.Label();
             this.lblTimer = new System.Windows.Forms.Label();
             this.btnClose = new System.Windows.Forms.Button();
-            this.gbSum = new System.Windows.Forms.GroupBox();
             this.lb_amount = new System.Windows.Forms.Label();
             this.lblTextSum = new System.Windows.Forms.Label();
             this.lblDutCost = new System.Windows.Forms.Label();
@@ -44,16 +43,18 @@
             this.time_close = new System.Windows.Forms.Timer(this.components);
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.gbDut = new System.Windows.Forms.GroupBox();
-            this.gbSum.SuspendLayout();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
-            this.gbDut.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblBill
             // 
             this.lblBill.AutoSize = true;
             this.lblBill.Font = new System.Drawing.Font("맑은 고딕", 20F);
+            this.lblBill.ForeColor = System.Drawing.Color.White;
             this.lblBill.Location = new System.Drawing.Point(16, 9);
             this.lblBill.Name = "lblBill";
             this.lblBill.Size = new System.Drawing.Size(98, 37);
@@ -64,6 +65,7 @@
             // 
             this.lblTimer.AutoSize = true;
             this.lblTimer.Font = new System.Drawing.Font("맑은 고딕", 10F);
+            this.lblTimer.ForeColor = System.Drawing.Color.White;
             this.lblTimer.Location = new System.Drawing.Point(453, 27);
             this.lblTimer.Name = "lblTimer";
             this.lblTimer.Size = new System.Drawing.Size(136, 19);
@@ -75,6 +77,7 @@
             this.btnClose.FlatAppearance.BorderSize = 0;
             this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClose.Font = new System.Drawing.Font("맑은 고딕", 13F);
+            this.btnClose.ForeColor = System.Drawing.Color.White;
             this.btnClose.Location = new System.Drawing.Point(614, 12);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(75, 37);
@@ -83,43 +86,38 @@
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
-            // gbSum
-            // 
-            this.gbSum.Controls.Add(this.lb_amount);
-            this.gbSum.Controls.Add(this.lblTextSum);
-            this.gbSum.Location = new System.Drawing.Point(23, 276);
-            this.gbSum.Name = "gbSum";
-            this.gbSum.Size = new System.Drawing.Size(442, 56);
-            this.gbSum.TabIndex = 4;
-            this.gbSum.TabStop = false;
-            // 
             // lb_amount
             // 
             this.lb_amount.AutoSize = true;
-            this.lb_amount.Font = new System.Drawing.Font("맑은 고딕", 9F);
-            this.lb_amount.Location = new System.Drawing.Point(400, 27);
+            this.lb_amount.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lb_amount.Font = new System.Drawing.Font("맑은 고딕", 15F);
+            this.lb_amount.ForeColor = System.Drawing.Color.White;
+            this.lb_amount.Location = new System.Drawing.Point(389, 15);
             this.lb_amount.Name = "lb_amount";
-            this.lb_amount.Size = new System.Drawing.Size(19, 15);
+            this.lb_amount.Size = new System.Drawing.Size(32, 28);
             this.lb_amount.TabIndex = 1;
             this.lb_amount.Text = "원";
             // 
             // lblTextSum
             // 
             this.lblTextSum.AutoSize = true;
-            this.lblTextSum.Font = new System.Drawing.Font("맑은 고딕", 9F);
-            this.lblTextSum.Location = new System.Drawing.Point(15, 27);
+            this.lblTextSum.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblTextSum.Font = new System.Drawing.Font("맑은 고딕", 15F);
+            this.lblTextSum.ForeColor = System.Drawing.Color.White;
+            this.lblTextSum.Location = new System.Drawing.Point(22, 15);
             this.lblTextSum.Name = "lblTextSum";
-            this.lblTextSum.Size = new System.Drawing.Size(31, 15);
+            this.lblTextSum.Size = new System.Drawing.Size(52, 28);
             this.lblTextSum.TabIndex = 0;
             this.lblTextSum.Text = "합계";
             // 
             // lblDutCost
             // 
             this.lblDutCost.AutoSize = true;
-            this.lblDutCost.Font = new System.Drawing.Font("맑은 고딕", 9F);
-            this.lblDutCost.Location = new System.Drawing.Point(94, 221);
+            this.lblDutCost.Font = new System.Drawing.Font("맑은 고딕", 25F);
+            this.lblDutCost.ForeColor = System.Drawing.Color.White;
+            this.lblDutCost.Location = new System.Drawing.Point(78, 211);
             this.lblDutCost.Name = "lblDutCost";
-            this.lblDutCost.Size = new System.Drawing.Size(19, 15);
+            this.lblDutCost.Size = new System.Drawing.Size(54, 46);
             this.lblDutCost.TabIndex = 11;
             this.lblDutCost.Text = "원";
             this.lblDutCost.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -127,18 +125,18 @@
             // lblDutNum
             // 
             this.lblDutNum.AutoSize = true;
-            this.lblDutNum.Font = new System.Drawing.Font("맑은 고딕", 9F);
-            this.lblDutNum.Location = new System.Drawing.Point(81, 175);
+            this.lblDutNum.Font = new System.Drawing.Font("맑은 고딕", 15F);
+            this.lblDutNum.ForeColor = System.Drawing.Color.White;
+            this.lblDutNum.Location = new System.Drawing.Point(71, 161);
             this.lblDutNum.Name = "lblDutNum";
-            this.lblDutNum.Size = new System.Drawing.Size(42, 15);
+            this.lblDutNum.Size = new System.Drawing.Size(70, 28);
             this.lblDutNum.TabIndex = 10;
             this.lblDutNum.Text = "1 인당";
             // 
             // btnPlus
             // 
             this.btnPlus.BackColor = System.Drawing.Color.White;
-            this.btnPlus.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnPlus.Location = new System.Drawing.Point(147, 93);
+            this.btnPlus.Location = new System.Drawing.Point(148, 101);
             this.btnPlus.Name = "btnPlus";
             this.btnPlus.Size = new System.Drawing.Size(30, 23);
             this.btnPlus.TabIndex = 9;
@@ -149,8 +147,7 @@
             // btnMinus
             // 
             this.btnMinus.BackColor = System.Drawing.Color.White;
-            this.btnMinus.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnMinus.Location = new System.Drawing.Point(24, 93);
+            this.btnMinus.Location = new System.Drawing.Point(25, 101);
             this.btnMinus.Name = "btnMinus";
             this.btnMinus.Size = new System.Drawing.Size(30, 23);
             this.btnMinus.TabIndex = 8;
@@ -161,20 +158,22 @@
             // lblNum
             // 
             this.lblNum.AutoSize = true;
-            this.lblNum.Font = new System.Drawing.Font("맑은 고딕", 9F);
-            this.lblNum.Location = new System.Drawing.Point(94, 98);
+            this.lblNum.Font = new System.Drawing.Font("맑은 고딕", 18F);
+            this.lblNum.ForeColor = System.Drawing.Color.White;
+            this.lblNum.Location = new System.Drawing.Point(89, 92);
             this.lblNum.Name = "lblNum";
-            this.lblNum.Size = new System.Drawing.Size(14, 15);
+            this.lblNum.Size = new System.Drawing.Size(28, 32);
             this.lblNum.TabIndex = 7;
             this.lblNum.Text = "1";
             // 
             // lblTextDut
             // 
             this.lblTextDut.AutoSize = true;
-            this.lblTextDut.Font = new System.Drawing.Font("맑은 고딕", 9F);
-            this.lblTextDut.Location = new System.Drawing.Point(71, 36);
+            this.lblTextDut.Font = new System.Drawing.Font("맑은 고딕", 23F);
+            this.lblTextDut.ForeColor = System.Drawing.Color.White;
+            this.lblTextDut.Location = new System.Drawing.Point(32, 17);
             this.lblTextDut.Name = "lblTextDut";
-            this.lblTextDut.Size = new System.Drawing.Size(55, 15);
+            this.lblTextDut.Size = new System.Drawing.Size(142, 42);
             this.lblTextDut.TabIndex = 6;
             this.lblTextDut.Text = "더치페이";
             // 
@@ -185,6 +184,7 @@
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.panel1.Controls.Add(this.btnClose);
             this.panel1.Controls.Add(this.lblTimer);
             this.panel1.Controls.Add(this.lblBill);
@@ -196,36 +196,47 @@
             // 
             // panel2
             // 
-            this.panel2.Location = new System.Drawing.Point(23, 62);
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(23, 69);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(442, 208);
             this.panel2.TabIndex = 7;
             // 
-            // gbDut
+            // panel3
             // 
-            this.gbDut.Controls.Add(this.lblDutCost);
-            this.gbDut.Controls.Add(this.lblDutNum);
-            this.gbDut.Controls.Add(this.btnPlus);
-            this.gbDut.Controls.Add(this.btnMinus);
-            this.gbDut.Controls.Add(this.lblNum);
-            this.gbDut.Controls.Add(this.lblTextDut);
-            this.gbDut.Location = new System.Drawing.Point(481, 55);
-            this.gbDut.Name = "gbDut";
-            this.gbDut.Size = new System.Drawing.Size(200, 277);
-            this.gbDut.TabIndex = 5;
-            this.gbDut.TabStop = false;
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel3.Controls.Add(this.lb_amount);
+            this.panel3.Controls.Add(this.lblTextSum);
+            this.panel3.Location = new System.Drawing.Point(23, 290);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(442, 56);
+            this.panel3.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel4.Controls.Add(this.lblDutCost);
+            this.panel4.Controls.Add(this.lblDutNum);
+            this.panel4.Controls.Add(this.btnPlus);
+            this.panel4.Controls.Add(this.btnMinus);
+            this.panel4.Controls.Add(this.lblNum);
+            this.panel4.Controls.Add(this.lblTextDut);
+            this.panel4.Location = new System.Drawing.Point(484, 69);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 277);
+            this.panel4.TabIndex = 12;
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(700, 360);
             this.ControlBox = false;
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.gbDut);
-            this.Controls.Add(this.gbSum);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -233,12 +244,12 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "계산서";
             this.Load += new System.EventHandler(this.Form3_Load);
-            this.gbSum.ResumeLayout(false);
-            this.gbSum.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.gbDut.ResumeLayout(false);
-            this.gbDut.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -248,7 +259,6 @@
         private System.Windows.Forms.Label lblBill;
         private System.Windows.Forms.Label lblTimer;
         private System.Windows.Forms.Button btnClose;
-        private System.Windows.Forms.GroupBox gbSum;
         private System.Windows.Forms.Label lblDutCost;
         private System.Windows.Forms.Label lblDutNum;
         private System.Windows.Forms.Button btnPlus;
@@ -261,6 +271,7 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.GroupBox gbDut;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
     }
 }
